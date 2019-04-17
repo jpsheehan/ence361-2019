495c1614e45ffa291764475a47983bc4015ab9fd
Jesse Sheehan <jps111@uclive.ac.nz>
2019-04-17 17:49:32 +1200

    Fixed compilation errors in PWM files.

--- pwm.c
+++ pwm.c
@@ -1,3 +1,10 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+
 #include "pwm.h"
 
 static bool g_togglePB3 = false;
