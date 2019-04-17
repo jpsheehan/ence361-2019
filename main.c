8fea5b3e6e4ac6a31e316bc3fd9888f304b3dce9
Jesse Sheehan <jps111@uclive.ac.nz>
2019-04-17 18:00:01 +1200

    Fixed compilation errors in main file.

--- main.c
+++ main.c
@@ -18,21 +18,22 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-// #include "inc/hw_memmap.h"
-// #include "inc/hw_types.h"
-// #include "driverlib/adc.h"
-// #include "driverlib/pwm.h"
-// #include "driverlib/gpio.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/adc.h"
+#include "driverlib/pwm.h"
+#include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
-// #include "driverlib/systick.h"
-// #include "driverlib/interrupt.h"
-// #include "driverlib/debug.h"
+#include "driverlib/systick.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/debug.h"
 
 #include "button.h"
 #include "quadrature.h"
 #include "display.h"
 #include "altitude.h"
 #include "pwm.h"
+#include "utils.h"
 
 //*****************************************************************************
 // Initialisation functions for the clock (incl. SysTick), ADC, display
