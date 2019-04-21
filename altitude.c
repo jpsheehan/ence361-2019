d33ee1cbf99fc41eb24f67803c854dfab484abcb
Jesse Sheehan <jps111@uclive.ac.nz>
2019-04-22 10:16:53 +1200

    Added comments to the altitude module. Untested.

--- altitude.c
+++ altitude.c
@@ -1,7 +1,29 @@
+/*******************************************************************************
+ * 
+ * altitude.c
+ * 
+ * ENEL361 Helicopter Project
+ * Friday Morning, Group 7
+ * 
+ * Written by:
+ *  - Manu Hamblyn  <mfb31<@uclive.ac.nz>   95140875
+ *  - Will Cowper   <wgc22@uclive.ac.nz>    81163265
+ *  - Jesse Sheehan <jps111@uclive.ac.nz>   53366509
+ * 
+ * This file contains portions of code that written by P.J. Bones. These portions are noted in the comments.
+ * 
+ * Description:
+ * This module contains functionality required for calculating the mean altitude
+ * as a raw value and as a percentage of the overall height.
+ * Functions are provided to initialise, calibrate, update and return
+ * the altitude values.
+ * 
+ ******************************************************************************/
 
 #include <stdint.h>
 #include <stdbool.h>
 
+// TODO: See which includes can be safely removed
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
 #include "driverlib/adc.h"
@@ -15,22 +37,62 @@
 #include "circBufT.h"
 #include "altitude.h"
 
+/**
+ * The sample rate of the ADC. This is how many times per second the alt_ADCIntHandler is called.
+ */
 #define ALT_SAMPLE_RATE_HZ 256
+
+/**
+ * The size of the buffer used to store the raw ADC values. This needs to be big enough that outliers in the data cannot affect the calculated mean in an adverse way.
+ */
 #define ALT_BUF_SIZE 32
+
+/**
+ * The ideal resolution delta for a helicopter rig. This value may change depending on which helicopter rig is used. The ideal value is calculated as follows:
+ * 
+ * When the helicopter changes its height from landed (0% altitude) to full height (100% altitude) there is a voltage drop of 0.8 V. 
+ * 
+ * Because we are using a 12-bit ADC, that the maximum value we can read from the ADC is 2^12 - 1 (4095). The Tiva board uses 3.3 V as its supply voltage, so a resolution of 4095 corresponds to 3.3 V.
+ * 
+ * Hence the difference in 0.8 V corresponds to 4095 * 0.8 / 3.3 (993).
+ */
 #define ALT_DELTA 993
 
-static circBuf_t g_inBuffer;        // Buffer of size BUF_SIZE integers (sample values)
-static uint16_t g_altitudeReference; // the reference value for calculating height.
-static uint32_t g_latestAltitudeMean; // the latest altitude value
-static int32_t g_latestAltitudePercentage;
-static bool g_hasBeenCalibrated = false;
-static uint32_t g_ulSampCnt;    // Counter for the interrupts
+/**
+ * The circular buffer used to store the raw ADC values for calculating the mean.
+ */
+static circBuf_t g_inBuffer;
 
-//*****************************************************************************
-//
-// The handler for the ADC conversion complete interrupt.
-// Writes to the circular buffer.
-//*****************************************************************************
+/**
+ * The reference altitude. This is updated when calling the alt_calibrate function. This is required for calculating the altitude as a percentage.
+ */
+static uint16_t g_altitudeReference;
+
+/**
+ * The mean altitude. This is updated when the `void alt_update()` function is called.
+ */
+static uint32_t g_latestAltitudeMean;
+
+/**
+ * The mean altitude as a percentage of full height. This is updated when the `void alt_update()` function is called.
+ */
+static int32_t g_latestAltitudePercentage;
+
+/**
+ * Indicates if the altitude has been calibrated yet. This is set when calling the `void alt_calibrate()` function and returned when calling the `bool alt_getIsCalibrated()` function.
+ */
+static bool g_hasBeenCalibrated = false;
+
+/**
+ * Used as a counter for the number of samples taken. This is set whenever a conversion is initiated and is used by the `alt_getIsBufferFull()` function.
+ */
+static uint32_t g_ulSampCnt;
+
+/**
+ * (Original Code by P.J. Bones)
+ * The handler for the ADC conversion complete interrupt.
+ * Writes to the circular buffer.
+ */
 void alt_ADCIntHandler(void)
 {
     uint32_t ulValue;
@@ -41,13 +103,18 @@ void alt_ADCIntHandler(void)
     ADCSequenceDataGet(ADC0_BASE, 3, &ulValue);
     //
     // Place it in the circular buffer (advancing write index)
-    writeCircBuf (&g_inBuffer, ulValue);
+    writeCircBuf(&g_inBuffer, ulValue);
     //
     // Clean up, clearing the interrupt
     ADCIntClear(ADC0_BASE, 3);
 }
 
-void alt_initADC (void)
+/**
+ * (Original code by P.J. Bones)
+ * Initialises the ADC module on the Tivaboard.
+ * TODO: Give more information about the ADC we are using to perform the conversion.
+ */
+void alt_initADC(void)
 {
     //
     // The ADC0 peripheral must be enabled for configuration and use.
@@ -67,8 +134,7 @@ void alt_initADC (void)
     // sequence 0 has 8 programmable steps.  Since we are only doing a single
     // conversion using sequence 3 we will only configure step 0.  For more
     // on the ADC sequences and steps, refer to the LM3S1968 datasheet.
-    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH9 | ADC_CTL_IE |
-                             ADC_CTL_END);
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH9 | ADC_CTL_IE | ADC_CTL_END);
 
     //
     // Since sample sequence 3 is now configured, it must be enabled.
@@ -76,28 +142,32 @@ void alt_initADC (void)
 
     //
     // Register the interrupt handler
-    ADCIntRegister (ADC0_BASE, 3, alt_ADCIntHandler);
+    ADCIntRegister(ADC0_BASE, 3, alt_ADCIntHandler);
 
     //
     // Enable interrupts for ADC0 sequence 3 (clears any outstanding interrupts)
     ADCIntEnable(ADC0_BASE, 3);
 }
 
-//*****************************************************************************
-//
-// The interrupt handler for the for SysTick interrupt.
-//*****************************************************************************
-void
-alt_SysTickIntHandler(void)
+/**
+ * (Original code by P.J. Bones)
+ * The interrupt handler for the for SysTick interrupt.
+ */
+void alt_SysTickIntHandler(void)
 {
     //
     // Initiate a conversion
     //
-    ADCProcessorTrigger(ADC0_BASE, 3); 
+    ADCProcessorTrigger(ADC0_BASE, 3);
     g_ulSampCnt++;
 }
 
-void alt_initSysTick(void) {
+/**
+ * (Original code by P.J. Bones)
+ * Intialises the system tick interrupt handler.
+ */
+void alt_initSysTick(void)
+{
     //
     // Set up the period for the SysTick timer.  The SysTick timer period is
     // set as a function of the system clock.
@@ -111,48 +181,52 @@ void alt_initSysTick(void) {
     SysTickEnable();
 }
 
-void alt_init()
+void alt_init(void)
 {
     alt_initSysTick();
     alt_initADC();
-    initCircBuf (&g_inBuffer, ALT_BUF_SIZE);
+    initCircBuf(&g_inBuffer, ALT_BUF_SIZE);
 }
 
-void alt_update()
+void alt_update(void)
 {
     int32_t sum;
     uint16_t i;
 
+    // add up all the values in the circular buffer
     sum = 0;
     for (i = 0; i < ALT_BUF_SIZE; i++)
-        sum = sum + readCircBuf (&g_inBuffer);
+        sum = sum + readCircBuf(&g_inBuffer);
+
+    // calculate the mean of the data in the circular buffer
     g_latestAltitudeMean = (2 * sum + ALT_BUF_SIZE) / (2 * ALT_BUF_SIZE);
 
+    // calculate the percentage mean
     g_latestAltitudePercentage = ((((int32_t)g_altitudeReference - (int32_t)g_latestAltitudeMean) * (int32_t)100) / (int32_t)ALT_DELTA);
 }
 
-
-void alt_calibrate() {
+void alt_calibrate(void)
+{
     g_altitudeReference = g_latestAltitudeMean;
     g_hasBeenCalibrated = true;
 }
 
-int32_t alt_getPercent()
+int32_t alt_getPercent(void)
 {
     return g_latestAltitudePercentage;
 }
 
-uint32_t alt_getRaw()
+uint32_t alt_getRaw(void)
 {
     return g_latestAltitudeMean;
 }
 
-bool alt_getIsCalibrated()
+bool alt_getIsCalibrated(void)
 {
     return g_hasBeenCalibrated;
 }
 
-bool alt_getIsBufferFull()
+bool alt_getIsBufferFull(void)
 {
     return (g_ulSampCnt > ALT_SAMPLE_RATE_HZ);
 }
--- altitude.h
+++ altitude.h
@@ -1,9 +1,22 @@
-/*
+/*******************************************************************************
+ * 
  * altitude.h
- *
- *  Created on: 13/03/2019
- *      Author: jps111
- */
+ * 
+ * ENEL361 Helicopter Project
+ * Friday Morning, Group 7
+ * 
+ * Written by:
+ *  - Manu Hamblyn  <mfb31<@uclive.ac.nz>   95140875
+ *  - Will Cowper   <wgc22@uclive.ac.nz>    81163265
+ *  - Jesse Sheehan <jps111@uclive.ac.nz>   53366509
+ * 
+ * Description:
+ * This module contains functionality required for calculating the mean altitude
+ * as a raw value and as a percentage of the overall height.
+ * Functions are provided to initialise, calibrate, update and return
+ * the altitude values.
+ * 
+ ******************************************************************************/
 
 #ifndef ALTITUDE_H_
 #define ALTITUDE_H_
@@ -11,13 +24,41 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-void alt_init();
-void alt_update();
-void alt_calibrate();
+/**
+ * Initialises the altitude module.
+ * This must be called before any other functions in the altitude module.
+ */
+void alt_init(void);
 
-int32_t alt_getPercent();
-uint32_t alt_getRaw();
-bool alt_getIsCalibrated();
-bool alt_getIsBufferFull();
+/**
+ * Calculates the mean value of the altitude, both the raw resolution and the percentage values. This should be called before one wants to use the altitude values in any other calculations.
+ */
+void alt_update(void);
 
-#endif /* ALTITUDE_H_ */
+/**
+ * Calibrates the altitude to the current mean value.
+ * This must be called before calling `void alt_update(void)`.
+ */
+void alt_calibrate(void);
+
+/**
+ * Returns the mean altitude as a percentage (usually from 0 - 100). This value can be less than 0 or greater than 100.
+ */
+int32_t alt_getPercent(void);
+
+/**
+ * Returns the mean altitude. This is a value between 0 and 4095 (inclusive).
+ */
+uint32_t alt_getRaw(void);
+
+/**
+ * Returns `true` if the altitude has been calibrated.
+ */
+bool alt_getIsCalibrated(void);
+
+/**
+ * Returns `true` if the circular buffer is full and ready to be calculated.
+ */
+bool alt_getIsBufferFull(void);
+
+#endif
