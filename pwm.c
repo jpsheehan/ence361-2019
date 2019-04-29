f827939000e716ec785f71965b2a0cde32bbfab3
Jesse Sheehan <jps111@uclive.ac.nz>
2019-04-29 12:25:18 +1200

    Commented the PWM module. Added some macros. (untested)

--- pwm.c
+++ pwm.c
@@ -1,29 +1,54 @@
-#include <stdint.h>
-#include <stdbool.h>
-
-#include "inc/hw_memmap.h"
-#include "driverlib/gpio.h"
-#include "driverlib/sysctl.h"
+/*******************************************************************************
+ * 
+ * pwm.c
+ * 
+ * ENEL361 Helicopter Project
+ * Friday Morning, Group 7
+ * 
+ * Written by:
+ *  - Manu Hamblyn  <mfb31<@uclive.ac.nz>   95140875
+ *  - Will Cowper   <wgc22@uclive.ac.nz>    81163265
+ *  - Jesse Sheehan <jps111@uclive.ac.nz>   53366509
+ * 
+ * Description:
+ * This module contains functions for toggling a GPIO pin when desired. This
+ * can be used when the rate of something would like to be measured (e.g. the
+ * rate of the display redraw, etc).
+ * 
+ ******************************************************************************/
 
 #include "pwm.h"
 
-static bool g_togglePB3 = false;
+/**
+ * True when the output is high. This is used to keep track of the output.
+ */
+static bool g_toggled = false;
 
-void pwm_toggle()
+void pwm_toggle(void)
 {
-    // PB3
-    g_togglePB3 = !g_togglePB3;
+    // toggle the global variable (if it's off turn it on, and vice-versa)
+    g_toggled = !g_toggled;
 
-    if (g_togglePB3) {
-        GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_3, GPIO_PIN_3);
-    } else {
-        GPIOPinWrite(GPIO_PORTB_BASE,  GPIO_PIN_3, 0x00);
+    if (g_toggled)
+    {
+        // if the global is true, output a high signal
+        GPIOPinWrite(PWM_PORT_BASE, PWM_PORT_PIN, PWM_PORT_PIN);
+    }
+    else
+    {
+        // otherwise, output a low signal
+        GPIOPinWrite(PWM_PORT_BASE, PWM_PORT_PIN, 0x00);
     }
 }
 
-void pwm_init()
+void pwm_init(void)
 {
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-    GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_3, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPD);
-    GPIODirModeSet(GPIO_PORTB_BASE, GPIO_PIN_3, GPIO_DIR_MODE_OUT);
+    // initialise a port as a peripheral
+    SysCtlPeripheralEnable(PWM_PORT_PERIPH);
+
+    // configure the pin as WPD with a strength of 4 mA.
+    GPIOPadConfigSet(PWM_PORT_BASE, PWM_PORT_PIN, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPD);
+
+    // configure the pin as an output
+    GPIODirModeSet(PWM_PORT_BASE, PWM_PORT_PIN, GPIO_DIR_MODE_OUT);
 }
--- pwm.h
+++ pwm.h
@@ -1,7 +1,55 @@
+/*******************************************************************************
+ * 
+ * pwm.h
+ * 
+ * ENEL361 Helicopter Project
+ * Friday Morning, Group 7
+ * 
+ * Written by:
+ *  - Manu Hamblyn  <mfb31<@uclive.ac.nz>   95140875
+ *  - Will Cowper   <wgc22@uclive.ac.nz>    81163265
+ *  - Jesse Sheehan <jps111@uclive.ac.nz>   53366509
+ * 
+ * Description:
+ * This module contains functions for toggling a GPIO pin when desired. This
+ * can be used when the rate of something would like to be measured (e.g. the
+ * rate of the display redraw, etc).
+ * 
+ ******************************************************************************/
+
 #ifndef PWM_H_
 #define PWM_H_
 
-void pwm_init();
-void pwm_toggle();
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+
+/**
+ * The base of the pin, because we are using this for PB3, we want the B base.
+ */
+#define PWM_PORT_BASE GPIO_PORTB_BASE
+
+/**
+ * The pin number, we are using PB3 so we set it to pin 3.
+ */
+#define PWM_PORT_PIN GPIO_PIN_3
+
+/**
+ * The peripheral we must enable, in this case, the GPIO B peripheral.
+ */
+#define PWM_PORT_PERIPH SYSCTL_PERIPH_GPIOB
+
+/**
+ * Initialises the PWM module. This must be called before `pwm_toggle`.
+ */
+void pwm_init(void);
+
+/**
+ * Toggles the PWM value (if it's high, change it to low, etc).
+ */
+void pwm_toggle(void);
 
 #endif
