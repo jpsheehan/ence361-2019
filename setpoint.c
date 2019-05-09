021e8f85669499b2ca49e4fceebac8c4d34fab1f
Jesse Sheehan <jps111@uclive.ac.nz>
2019-05-10 09:56:16 +1200

    Commented the setpoint module.

--- setpoint.c
+++ setpoint.c
@@ -1,18 +1,44 @@
-/*
+/*******************************************************************************
+ *
  * setpoint.c
  *
- *  Created on: 9/05/2019
- *      Author: jps111
- */
+ * ENEL361 Helicopter Project
+ * Friday Morning, Group 7
+ *
+ * Written by:
+ *  - Manu Hamblyn  <mfb31<@uclive.ac.nz>   95140875
+ *  - Will Cowper   <wgc22@uclive.ac.nz>    81163265
+ *  - Jesse Sheehan <jps111@uclive.ac.nz>   53366509
+ *
+ * Description:
+ * This module contains functions for initialising, setting and getting desired
+ * yaw angles and altitude percentages. This will be used by the main loop (buttons)
+ * and the control module.
+ *
+ ******************************************************************************/
 
 #include "utils.h"
 #include "setpoint.h"
 
-static int16_t g_desired_yaw = 0;
-static int16_t g_desired_altitude = 0;
-
+/**
+ * The amount the yaw will change by for each increment/decrement (in degrees).
+ */
 static const int8_t YAW_DELTA = 15;
+
+/**
+ * The amount the altitude will change by for each increment/decrement (as a percentage).
+ */
 static const int8_t ALTITUDE_DELTA = 10;
+
+/**
+ * The target yaw value.
+ */
+static int16_t g_desired_yaw = 0;
+
+/**
+ * The target altitude value.
+ */
+static int16_t g_desired_altitude = 0;
 
 void setpoint_init(void)
 {
@@ -22,7 +48,8 @@ void setpoint_init(void)
 
 void setpoint_increment_yaw(void)
 {
-    g_desired_yaw = g_desired_yaw + YAW_DELTA;
+    // increment the yaw and then wrap around if necessary
+    g_desired_yaw += YAW_DELTA;
     if (g_desired_yaw >= 360)
     {
         g_desired_yaw -= 360;
@@ -31,7 +58,8 @@ void setpoint_increment_yaw(void)
 
 void setpoint_decrement_yaw(void)
 {
-    g_desired_yaw = g_desired_yaw - YAW_DELTA;
+    // decrement the yaw and then wrap around if necessary
+    g_desired_yaw -= YAW_DELTA;
     if (g_desired_yaw < 0)
     {
         g_desired_yaw += 360;
@@ -45,11 +73,13 @@ int16_t setpoint_get_yaw(void)
 
 void setpoint_increment_altitude(void)
 {
+    // increment the altitude and then clamp if necessary
     g_desired_altitude = min(g_desired_altitude + ALTITUDE_DELTA, 100);
 }
 
 void setpoint_decrement_altitude(void)
 {
+    // decrement the altitude and then clamp if necessary
     g_desired_altitude = max(g_desired_altitude - ALTITUDE_DELTA, 0);
 }
 
--- setpoint.h
+++ setpoint.h
@@ -1,23 +1,60 @@
-/*
+/*******************************************************************************
+ *
  * setpoint.h
  *
- *  Created on: 9/05/2019
- *      Author: jps111
- */
+ * ENEL361 Helicopter Project
+ * Friday Morning, Group 7
+ *
+ * Written by:
+ *  - Manu Hamblyn  <mfb31<@uclive.ac.nz>   95140875
+ *  - Will Cowper   <wgc22@uclive.ac.nz>    81163265
+ *  - Jesse Sheehan <jps111@uclive.ac.nz>   53366509
+ *
+ * Description:
+ * This module contains functions for initialising, setting and getting desired
+ * yaw angles and altitude percentages. This will be used by the main loop (buttons)
+ * and the control module.
+ *
+ ******************************************************************************/
 
 #ifndef SETPOINT_H_
 #define SETPOINT_H_
 
 #include <stdint.h>
 
+/**
+ * Initialises the setpoint values to 0.
+ */
 void setpoint_init(void);
 
+/**
+ * Increments the target yaw by a certain amount.
+ */
 void setpoint_increment_yaw(void);
+
+/**
+ * Decrements the target yaw by a certain amount.
+ */
 void setpoint_decrement_yaw(void);
+
+/**
+ * Returns the target yaw (expressed in degrees).
+ */
 int16_t setpoint_get_yaw(void);
 
+/**
+ * Increments the target altitude by a certain amount.
+ */
 void setpoint_increment_altitude(void);
+
+/**
+ * Decrements the target altitude by a certain amount.
+ */
 void setpoint_decrement_altitude(void);
+
+/**
+ * Returns the target altitude (expressed as a percentage of the total height).
+ */
 int16_t setpoint_get_altitude(void);
 
 #endif /* SETPOINT_H_ */
