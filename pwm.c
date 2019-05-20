ae90bbfc4d66d029c38361aa18ae3ac93b1b2a22
Jesse Sheehan <jps111@uclive.ac.nz>
2019-05-20 22:59:50 +1200

    Refactored some constants out

--- pwm.c
+++ pwm.c
@@ -52,41 +52,40 @@
 /**********************************************************
  * Constants
  **********************************************************/
-// Systick configuration
-#define SYSTICK_RATE_HZ 100
 
 // PWM configuration
-#define PWM_RATE 200
-#define PWM_INITIAL_DUTY 0
-#define PWM_DIVIDER_CODE SYSCTL_PWMDIV_8 //40MHz system clk
-#define PWM_DIVIDER 8
+static const int PWM_RATE = 200;
+static const int PWM_DIVIDER_CODE = SYSCTL_PWMDIV_8; //40MHz system clk
+static const int PWM_DIVIDER = 8;
 
 //  PWM Hardware Details M0PWM7 (gen 3)
 //  ---Main Rotor PWM: PC5, J4-05
-#define PWM_MAIN_BASE PWM0_BASE   //PWM module 0
-#define PWM_MAIN_GEN PWM_GEN_3    //PWM generator
-#define PWM_MAIN_OUTNUM PWM_OUT_7 //outputs 7 (& 6) correspond to GEN_3
-#define PWM_MAIN_OUTBIT PWM_OUT_7_BIT
-#define PWM_MAIN_PERIPH_PWM SYSCTL_PERIPH_PWM0 //PWM module 0
-#define PWM_MAIN_PERIPH_GPIO SYSCTL_PERIPH_GPIOC
-#define PWM_MAIN_GPIO_BASE GPIO_PORTC_BASE
-#define PWM_MAIN_GPIO_CONFIG GPIO_PC5_M0PWM7
-#define PWM_MAIN_GPIO_PIN GPIO_PIN_5
+static const int PWM_MAIN_BASE = PWM0_BASE;   //PWM module 0
+static const int PWM_MAIN_GEN = PWM_GEN_3;    //PWM generator
+static const int PWM_MAIN_OUTNUM = PWM_OUT_7; //outputs 7 (& 6) correspond to GEN_3
+static const int PWM_MAIN_OUTBIT = PWM_OUT_7_BIT;
+static const int PWM_MAIN_PERIPH_PWM = SYSCTL_PERIPH_PWM0; //PWM module 0
+static const int PWM_MAIN_PERIPH_GPIO = SYSCTL_PERIPH_GPIOC;
+static const int PWM_MAIN_GPIO_BASE = GPIO_PORTC_BASE;
+static const int PWM_MAIN_GPIO_CONFIG = GPIO_PC5_M0PWM7;
+static const int PWM_MAIN_GPIO_PIN = GPIO_PIN_5;
 
 //  PWM Hardware Details M1PWM5 (gen 5)
 //  ---Tail Rotor PWM: PF1, J3-10
-#define PWM_TAIL_BASE PWM1_BASE   //PWM module 1
-#define PWM_TAIL_GEN PWM_GEN_2    //PWM generator
-#define PWM_TAIL_OUTNUM PWM_OUT_5 //outputs 5 (& 4) correspond to GEN_2
-#define PWM_TAIL_OUTBIT PWM_OUT_5_BIT
-#define PWM_TAIL_PERIPH_PWM SYSCTL_PERIPH_PWM1 //PWM module 1
-#define PWM_TAIL_PERIPH_GPIO SYSCTL_PERIPH_GPIOF
-#define PWM_TAIL_GPIO_BASE GPIO_PORTF_BASE   //GPIO port
-#define PWM_TAIL_GPIO_CONFIG GPIO_PF1_M1PWM5 //PWM module 1, PWM o/p 5
-#define PWM_TAIL_GPIO_PIN GPIO_PIN_1         //GPIO pin
+static const int PWM_TAIL_BASE = PWM1_BASE;   //PWM module 1
+static const int PWM_TAIL_GEN = PWM_GEN_2;    //PWM generator
+static const int PWM_TAIL_OUTNUM = PWM_OUT_5; //outputs 5 (& 4) correspond to GEN_2
+static const int PWM_TAIL_OUTBIT = PWM_OUT_5_BIT;
+static const int PWM_TAIL_PERIPH_PWM = SYSCTL_PERIPH_PWM1; //PWM module 1
+static const int PWM_TAIL_PERIPH_GPIO = SYSCTL_PERIPH_GPIOF;
+static const int PWM_TAIL_GPIO_BASE = GPIO_PORTF_BASE;   //GPIO port
+static const int PWM_TAIL_GPIO_CONFIG = GPIO_PF1_M1PWM5; //PWM module 1, PWM o/p 5
+static const int PWM_TAIL_GPIO_PIN = GPIO_PIN_1;         //GPIO pin
 
 static int8_t g_main_duty;
 static int8_t g_tail_duty;
+
+static uint32_t g_pwm_period;
 
 /*********************************************************
  * initialisePWM
@@ -94,6 +93,8 @@ static int8_t g_tail_duty;
  *********************************************************/
 void pwm_init(void)
 {
+    g_pwm_period = SysCtlClockGet() / (PWM_DIVIDER * PWM_RATE);
+
     SysCtlPWMClockSet(PWM_DIVIDER_CODE);
 
     // initialise the main rotor
@@ -106,7 +107,8 @@ void pwm_init(void)
     PWMGenConfigure(PWM_MAIN_BASE, PWM_MAIN_GEN,
                     PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
     // Set the initial PWM parameters
-    pwm_set_main_duty(PWM_INITIAL_DUTY);
+    PWMGenPeriodSet(PWM_MAIN_BASE, PWM_MAIN_GEN, g_pwm_period);
+    pwm_set_main_duty(0);
 
     PWMGenEnable(PWM_MAIN_BASE, PWM_MAIN_GEN);
 
@@ -121,8 +123,10 @@ void pwm_init(void)
 
     PWMGenConfigure(PWM_TAIL_BASE, PWM_TAIL_GEN,
                     PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
+
     // Set the initial PWM parameters
-    pwm_set_tail_duty(PWM_INITIAL_DUTY);
+    PWMGenPeriodSet(PWM_TAIL_BASE, PWM_TAIL_GEN, g_pwm_period);
+    pwm_set_tail_duty(0);
 
     PWMGenEnable(PWM_TAIL_BASE, PWM_TAIL_GEN);
 
@@ -135,13 +139,8 @@ void pwm_init(void)
 void pwm_set_main_duty(int8_t t_duty)
 {
     g_main_duty = clamp(t_duty, 0, 100);
-
-    // Calculate the PWM period corresponding to the freq.
-    uint32_t ui32Period = SysCtlClockGet() / PWM_DIVIDER / PWM_RATE;
-
-    PWMGenPeriodSet(PWM_MAIN_BASE, PWM_MAIN_GEN, ui32Period);
     PWMPulseWidthSet(PWM_MAIN_BASE, PWM_MAIN_OUTNUM,
-                     ui32Period * g_main_duty / 100);
+                     g_pwm_period * g_main_duty / 100);
 }
 
 int8_t pwm_get_main_duty(void)
@@ -155,13 +154,8 @@ int8_t pwm_get_main_duty(void)
 void pwm_set_tail_duty(int8_t t_duty)
 {
     g_tail_duty = clamp(t_duty, 0, 100);
-
-    // Calculate the PWM period corresponding to the freq.
-    uint32_t ui32Period = SysCtlClockGet() / PWM_DIVIDER / PWM_RATE;
-
-    PWMGenPeriodSet(PWM_TAIL_BASE, PWM_TAIL_GEN, ui32Period);
     PWMPulseWidthSet(PWM_TAIL_BASE, PWM_TAIL_OUTNUM,
-                     ui32Period * g_tail_duty / 100);
+                     g_pwm_period * g_tail_duty / 100);
 }
 
 int8_t pwm_get_tail_duty(void)
