336f8ed40b9d03072809537a4e68d577eb251e46
Jesse Sheehan <jps111@uclive.ac.nz>
2019-05-20 22:59:38 +1200

    Did some reworking of yaw stuff

--- yaw.c
+++ yaw.c
@@ -65,6 +65,34 @@ static bool g_has_been_calibrated;
  */
 static const int YAW_MAX_SLOT_COUNT = 448;
 
+/**
+ * Yaw Quadrature Encoding:
+ * PB0 is the Phase A pin
+ * PB1 is the Phase B pin
+ */
+static const int YAW_QUAD_PERIPH = SYSCTL_PERIPH_GPIOB;
+static const int YAW_QUAD_BASE = GPIO_PORTB_BASE;
+static const int YAW_QUAD_INT_PIN_1 = GPIO_INT_PIN_0;
+static const int YAW_QUAD_INT_PIN_2 = GPIO_INT_PIN_1;
+static const int YAW_QUAD_PIN_1 = GPIO_PIN_0;
+static const int YAW_QUAD_PIN_2 = GPIO_PIN_1;
+static const int YAW_QUAD_SIG_STRENGTH = GPIO_STRENGTH_2MA;
+static const int YAW_QUAD_PIN_TYPE = GPIO_PIN_TYPE_STD_WPD;
+static const int YAW_QUAD_EDGE_TYPE = GPIO_BOTH_EDGES;
+
+/**
+ * Yaw Reference:
+ * PC4 is the reference pin
+ */
+static const int YAW_REF_PERIPH = SYSCTL_PERIPH_GPIOC;
+static const int YAW_REF_BASE = GPIO_PORTC_BASE;
+static const int YAW_REF_INT_PIN = GPIO_INT_PIN_4;
+static const int YAW_REF_PIN = GPIO_PIN_4;
+static const int YAW_REF_SIG_STRENGTH = GPIO_STRENGTH_2MA;
+static const int YAW_REF_PIN_TYPE = GPIO_PIN_TYPE_STD_WPD;
+static const int YAW_REF_EDGE_TYPE = GPIO_FALLING_EDGE;
+
+
 // prototypes for functions local to the yaw module
 void yaw_update_state(bool t_signal_a, bool t_signal_b);
 void yaw_int_handler(void);
@@ -79,50 +107,47 @@ void yaw_init(void)
     g_slot_count = 0;
     
     // setup the pins (PB0 is A, PB1 is B)
-    SysCtlPeripheralEnable (SYSCTL_PERIPH_GPIOB);
+    SysCtlPeripheralEnable(YAW_QUAD_PERIPH);
 	
 	// disable interrupts for safety
-    GPIOIntDisable(GPIO_PORTB_BASE, GPIO_INT_PIN_0 | GPIO_INT_PIN_1);
+    GPIOIntDisable(YAW_QUAD_BASE, YAW_QUAD_INT_PIN_1 | YAW_QUAD_INT_PIN_2);
 	
 	// Set the GPIO pins as inputs
-    GPIOPinTypeGPIOInput (GPIO_PORTB_BASE, GPIO_PIN_0|GPIO_PIN_1);
+    GPIOPinTypeGPIOInput(YAW_QUAD_BASE, YAW_QUAD_PIN_1 | YAW_QUAD_PIN_2);
 	
 	// Set the GPIO pins Weak Pull Down, 2mA
-    GPIOPadConfigSet (GPIO_PORTB_BASE, GPIO_PIN_0|GPIO_PIN_1, GPIO_STRENGTH_2MA,
-       GPIO_PIN_TYPE_STD_WPD);
+    GPIOPadConfigSet(YAW_QUAD_BASE, YAW_QUAD_PIN_1 | YAW_QUAD_PIN_2, YAW_QUAD_SIG_STRENGTH, YAW_QUAD_PIN_TYPE);
 	   
 	// Set the GPIO pins to generate interrupts on both rising and falling edges
-    GPIOIntTypeSet(GPIO_PORTB_BASE, GPIO_PIN_0|GPIO_PIN_1, GPIO_BOTH_EDGES);
+    GPIOIntTypeSet(YAW_QUAD_BASE, YAW_QUAD_PIN_1 | YAW_QUAD_PIN_2, YAW_QUAD_EDGE_TYPE);
 
 	// Register the interrupt handler
-    GPIOIntRegister(GPIO_PORTB_BASE, yaw_int_handler);
+    GPIOIntRegister(YAW_QUAD_BASE, yaw_int_handler);
 	
 	// Enable interrupts on GPIO Port B Pins 0,1 for Yaw channels A and B
 	// (clears any outstanding interrupts)
-    GPIOIntEnable(GPIO_PORTB_BASE, GPIO_INT_PIN_0 | GPIO_INT_PIN_1);
-
-    // PC4 is the yaw reference, set it up as an input with a falling edge interrupt trigger
+    GPIOIntEnable(YAW_QUAD_BASE, YAW_QUAD_INT_PIN_1 | YAW_QUAD_INT_PIN_2);
 
     // enable the peripheral
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    SysCtlPeripheralEnable(YAW_REF_PERIPH);
 
     // disable interrups
-    GPIOIntDisable(GPIO_PORTC_BASE, GPIO_INT_PIN_4);
+    GPIOIntDisable(YAW_REF_BASE, YAW_REF_INT_PIN);
 
     // set it up as an input
-    GPIOPinTypeGPIOInput(GPIO_PORTC_BASE, GPIO_PIN_4);
+    GPIOPinTypeGPIOInput(YAW_REF_BASE, YAW_REF_PIN);
 
     // configure it to be a weak pull down
-    GPIOPadConfigSet(GPIO_PORTC_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
+    GPIOPadConfigSet(YAW_REF_BASE, YAW_REF_PIN, YAW_REF_SIG_STRENGTH, YAW_REF_PIN_TYPE);
 
     // configure the interrupt to be falling edge only
-    GPIOIntTypeSet(GPIO_PORTC_BASE, GPIO_PIN_4, GPIO_FALLING_EDGE);
+    GPIOIntTypeSet(YAW_REF_BASE, YAW_REF_PIN, YAW_REF_EDGE_TYPE);
 
     // register the interrupt handler
-    GPIOIntRegister(GPIO_PORTC_BASE, yaw_reference_int_handler);
+    GPIOIntRegister(YAW_REF_BASE, yaw_reference_int_handler);
 
     // enable interrupts on this pin
-    GPIOIntEnable(GPIO_PORTC_BASE, GPIO_INT_PIN_4);
+    GPIOIntEnable(YAW_REF_BASE, YAW_REF_INT_PIN);
 }
 
 /**
