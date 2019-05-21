28b1cc054882c812ad0824242a5bfb6545580b3a
Jesse Sheehan <jps111@uclive.ac.nz>
2019-05-22 11:41:33 +1200

    Refactored code in main.c

--- main.c
+++ main.c
@@ -18,33 +18,62 @@
  *
  ******************************************************************************/
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
 
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-#include "driverlib/adc.h"
-#include "driverlib/gpio.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/systick.h"
 #include "driverlib/interrupt.h"
-#include "driverlib/debug.h"
 
-#include "display.h"
 #include "altitude.h"
-#include "utils.h"
-#include "uart.h"
-#include "yaw.h"
+#include "clock.h"
+#include "control.h"
+#include "display.h"
+#include "flight_mode.h"
+#include "input.h"
+#include "kernel.h"
 #include "pwm.h"
 #include "setpoint.h"
-#include "kernel.h"
-#include "flight_mode.h"
-#include "control.h"
-#include "input.h"
-#include "clock.h"
+#include "uart.h"
+#include "utils.h"
+#include "yaw.h"
 
+/**
+ * The "frequency" that the kernel runs at in Hz.
+ */
 static const int KERNEL_FREQUENCY = 1024;
+
+// process ADC stuff 256 times per second
+static const uint16_t ALT_ADC_FREQUENCY = 256;
+static const uint8_t ALT_ADC_PRIORITY = 1;
+
+// always update the altitude
+static const uint16_t ALT_CALC_FREQUENCY = 0;
+static const uint8_t ALT_CALC_PRIORITY = 1;
+
+// always process input
+static const uint16_t INPUT_FREQUENCY = 0;
+static const uint8_t INPUT_PRIORITY = 2;
+
+// perform altitude control stuff 30 times per second
+static const uint16_t CONTROL_ALT_FREQUENCY = 30;
+static const uint8_t CONTROL_ALT_PRIORITY = 10;
+
+// perform yaw control stuff 30 times per second
+static const uint16_t CONTROL_YAW_FREQUENCY = 30;
+static const uint8_t CONTROL_YAW_PRIORITY = 10;
+
+// run state checking 20 times per sec
+static const uint16_t FLIGHT_MODE_FREQUENCY = 20;
+static const uint8_t FLIGHT_MODE_PRIORITY = 10;
+
+// update the screen once per second
+static const uint16_t DISPLAY_FREQUENCY = 1;
+static const uint8_t DISPLAY_PRIORITY = 100;
+
+// update the UART four times per second
+static const uint16_t UART_FREQUENCY = 4;
+static const uint8_t UART_PRIORITY = 100;
+
 
 void initialise(void)
 {
@@ -69,14 +98,14 @@ void initialise(void)
                  (ControlGains){0.5f, 0.006f, 1.5f}); // P, I and D gains for tail rotor
 
     // add tasks to the kernel
-    kernel_add_task(&alt_process_adc, 256, 1);         // process ADC stuff 256 times per second
-    kernel_add_task(&alt_update, 0, 1);                // always update the altitude
-    kernel_add_task(&input_update, 0, 2);              // always process input
-    kernel_add_task(&control_update_altitude, 30, 10); // perform control stuff 10 times per second
-    kernel_add_task(&control_update_yaw, 30, 10);
-    kernel_add_task(&flight_mode_update, 20, 10); // run state checking 20 times per sec
-    kernel_add_task(&disp_render, 1, 100);        // update the screen once per second
-    kernel_add_task(&uart_update, 4, 100);        // update the UART four times per second
+    kernel_add_task(&alt_process_adc, ALT_ADC_FREQUENCY, ALT_ADC_PRIORITY);
+    kernel_add_task(&alt_update, ALT_CALC_FREQUENCY, ALT_CALC_PRIORITY);
+    kernel_add_task(&input_update, INPUT_FREQUENCY, INPUT_PRIORITY);
+    kernel_add_task(&control_update_altitude, CONTROL_ALT_FREQUENCY, CONTROL_ALT_PRIORITY);
+    kernel_add_task(&control_update_yaw, CONTROL_YAW_FREQUENCY, CONTROL_YAW_PRIORITY);
+    kernel_add_task(&flight_mode_update, FLIGHT_MODE_FREQUENCY, FLIGHT_MODE_PRIORITY);
+    kernel_add_task(&disp_render, DISPLAY_FREQUENCY, DISPLAY_PRIORITY);
+    kernel_add_task(&uart_update, UART_FREQUENCY, UART_PRIORITY);
 
     // Enable interrupts to the processor.
     IntMasterEnable();
