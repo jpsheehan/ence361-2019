7b14c4c168a3d6c87f01ff516440dccb839ec13a
Jesse Sheehan <jps111@uclive.ac.nz>
2019-05-28 07:47:07 +1200

    Added comment block to top of clock module

--- clock.c
+++ clock.c
@@ -1,10 +1,23 @@
-/*
+/*******************************************************************************
+ *
  * clock.c
  *
- *  Created on: 21/05/2019
- *      Author: jps111
- */
-
+ * ENEL361 Helicopter Project
+ * Friday Morning, Group 7
+ *
+ * Written by:
+ *  - Manu Hamblyn  <mfb31<@uclive.ac.nz>   95140875
+ *  - Will Cowper   <wgc22@uclive.ac.nz>    81163265
+ *  - Jesse Sheehan <jps111@uclive.ac.nz>   53366509
+ *
+ * This file contains portions of code that written by P.J. Bones.
+ * These portions are noted in the comments.
+ *
+ * Description:
+ * Contains the clock initialisation function which is responsible for setting
+ * the system clock to run at 40 MHz.
+ *
+ ******************************************************************************/
 
 #include <stdint.h>
 #include <stdbool.h>
--- clock.h
+++ clock.h
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ *
+ * clock.h
+ *
+ * ENEL361 Helicopter Project
+ * Friday Morning, Group 7
+ *
+ * Written by:
+ *  - Manu Hamblyn  <mfb31<@uclive.ac.nz>   95140875
+ *  - Will Cowper   <wgc22@uclive.ac.nz>    81163265
+ *  - Jesse Sheehan <jps111@uclive.ac.nz>   53366509
+ *
+ * This file contains portions of code that written by P.J. Bones.
+ * These portions are noted in the comments.
+ *
+ * Description:
+ * Contains the clock initialisation function which is responsible for setting
+ * the system clock to run at 40 MHz.
+ *
+ ******************************************************************************/
+
 #ifndef CLOCK_H_
 #define CLOCK_H_
 
