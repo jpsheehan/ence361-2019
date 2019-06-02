3bf30b453db87dd8f7b017fd9ac0695416c1c351
Jesse Sheehan <jps111@uclive.ac.nz>
2019-06-02 13:42:45 +1200

    Added better CPU utilization graph

--- tools/cpu_utilization_from_log.py
+++ tools/cpu_utilization_from_log.py
@@ -10,83 +10,19 @@ Website: electronut.in
 import sys
 import argparse
 import numpy as np
-from time import sleep
-from collections import deque
 
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 
-# plot class
-class AnalogPlot:
-    # constr
-    def __init__(self, filename, maxLen):
-        # open file
-        self.file = open(filename, 'r')
-        self.a = deque([0.0]*maxLen)
-        self.all = {}
-        self.has_made_pi_chart = False
-        self.maxLen = maxLen
+def extract_utilization(data, length, t=None):
+    u = np.zeros(length)
 
-    # add to buffer
-    def addToBuf(self, buf, val):
-        if len(buf) < self.maxLen:
-            buf.append(val)
-        else:
-            buf.pop()
-            buf.appendleft(val)
-
-    # add data
-    def add(self, data):
-        total_utilization = 0
-        for i in range(len(data)):
-
-            utilization = 0
-            if data[i][3] != 0:
-                # duration * frequency / 1000000.0 * 100
-                utilization = data[i][1] * data[i][3] / 10000.0
-            if data[i][0] != "uart_kernel_data":
-                total_utilization += utilization
-
-            if data[i][0] not in self.all:
-                self.all[data[i][0]] = []
-            self.all[data[i][0]].append(utilization)
-        self.addToBuf(self.a, total_utilization)
-        print(total_utilization)
-
-    def make_pi_chart(self):
-        averages = {}
-        for key in self.all:
-            averages[key] = sum(self.all[key]) / len(self.all[key])
-            print("{}: {}%".format(key, averages[key]))
-
-    def update(self, frameNum, a):
-        try:
-            line = self.file.readline()
-            if line == "" and self.has_made_pi_chart == False:
-                self.make_pi_chart()
-                self.has_made_pi_chart = True
-
-            elif "altitude" in line:
-                valid_rows = line.strip().split()[:-1]
-                raw_data = [row.split(',') for row in valid_rows]
-                if len(raw_data) * 4 == sum([len(data) for data in raw_data]):
-                    parsed_data = [(name, int(duration), int(period), int(frequency)) for (
-                        name, duration, period, frequency) in raw_data]
-                    self.add(parsed_data)
-                    for _i in range(len(parsed_data)):
-                        a.set_data(range(self.maxLen), self.a)
-        except KeyboardInterrupt:
-            print('exiting')
-
-        return a
-
-    # clean up
-    def close(self):
-        # close file
-        self.file.close()
-
-# main() function
+    for key in data:
+        if t is None or key == t:
+            for i, datum in enumerate(data[key]):
+                utilization = datum[3]
+                u[i] += utilization
+    return u
 
 
 def main():
@@ -102,29 +38,68 @@ def main():
 
     print('reading from filename %s...' % filename)
 
-    # plot parameters
-    analogPlot = AnalogPlot(filename, 200)
+    file = open(filename)
 
-    print('plotting data...')
+    data = {}
 
-    # set up animation
+    ignore_uart_kernel = True
+
+    # crunch the data
+    for line in file.readlines():
+        if "altitude" in line:
+            valid_rows = line.strip().split()
+            raw_data = [row.split(',') for row in valid_rows]
+            if len(raw_data) * 4 == sum([len(data) for data in raw_data]):
+                for (name, duration, period, frequency) in [(name, int(duration), int(period), int(frequency)) for (
+                        name, duration, period, frequency) in raw_data]:
+
+                    # ignore the kernel uart task if specified
+                    if name == "uart_kernel_data" and ignore_uart_kernel:
+                        continue
+
+                    # add an array when the key doesn't exist
+                    if name not in data:
+                        data[name] = []
+
+                    # calculate the % time utilization
+                    utilization = duration * frequency / 10000.0
+
+                    # calculate the time error (in micros)
+                    time_error_us = 0
+                    if frequency != 0:
+                        time_error_us = abs(1.0 / frequency * 1000000 - period)
+
+                    # calculate time error (as a % of period)
+                    time_error = time_error_us / period * 100.0
+
+                    data[name].append(
+                        (duration, period, frequency, utilization, time_error))
+
+    file.close()
+
+    # print(data)
+
+    n = len(data["uart_flight_data"])
+
+    # plot the data
     fig = plt.figure()
-    ax = plt.axes(xlim=(0, 200), ylim=(0, 20))
-    a = ax.plot([], [])
-    plt.title = "Kernel Task Efficiency"
-    plt.xlabel("- Time (s)")
-    plt.ylabel("CPU Utilization (%)")
-    anim = animation.FuncAnimation(fig, analogPlot.update,
-                                   fargs=a,
-                                   interval=50)
 
-    # show plot
+    # replace underscores with spaces, capitalize each word
+    pretty_keys = [' '.join([x.capitalize() for x in s.split('_')])
+                   for s in data.keys()]
+
+    # CPU Utilization
+    total_utilization = extract_utilization(data, n)
+    ymax = round(max(total_utilization) / 10.0) * 10
+    cpu_util = plt.axes(xlim=(0, n), ylim=(0, ymax),
+                        title="Kernel Task Time", xlabel="TIme (s)", ylabel="CPU Time Utilization (%)")
+    cpu_util.plot(range(n), total_utilization)
+
+    for key in data:
+        cpu_util.plot(range(n), extract_utilization(data, n, key))
+    cpu_util.legend(labels=["Total"] + pretty_keys, loc="upper left")
+
     plt.show()
-
-    # clean up
-    analogPlot.close()
-
-    print('exiting.')
 
 
 # call main
